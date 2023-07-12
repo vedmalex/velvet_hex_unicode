7ce2fc033b56bf1097bbe8be1de458f8c8e3bc08
vedmalex <vedmalex@gmail.com>
2023-07-12 13:24:35 +0300

    forma

--- keymap.c
+++ keymap.c
@@ -4,8 +4,6 @@
 #include "swapper.h"
 #include "keymap_russian.h"
 
-// #include "unicode_sym.h"
-
 #define RESET QK_BOOT
 
 #define LA_SYM MO(_SYM)
@@ -13,7 +11,8 @@
 #define LA_ALT MO(_ALT)
 
 #define DF_RU DF(_RU)
-#define DF_EN DF(_EN)
+#define DF_EN DF(_EN) oklkjyuuoynmnjnjh
+#define DF_MOUSE TG(_MOUSE)
 
 #define QUOT S(KC_GRV)
 #define PIPE S(KC_BSLS)
@@ -38,6 +37,7 @@ enum layers {
     _NAV,
     _NUM,
     _ALT,
+    _MOUSE,
 };
 
 enum keycodes {
@@ -60,48 +60,57 @@ enum keycodes {
     JS_ARROW,     // =>
 };
 
+#define KC_DQT S(KC_QUOT)
+#define KC_USCR S(KC_MINS)
+#define KC_CLN S(KC_SCLN)
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_EN] = LAYOUT_all(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M, S(KC_7), KC_LBRC, KC_RBRC,
-                DF_RU, SW_LANG,  KC_SPC,  LA_NAV,         LA_SYM, KC_LSFT,  LA_ALT, _______
+      KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
+      KC_A,    KC_S,   KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,
+      KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,          KC_N,   KC_M,S(KC_7),KC_LBRC,KC_RBRC,
+           DF_MOUSE,SW_LANG, KC_SPC, LA_NAV,        LA_SYM,KC_LSFT, LA_ALT,  DF_RU
     ),
 
     [_RU] = LAYOUT_all(
-     RU_SHTI,  RU_TSE,    RU_U,   RU_KA,   RU_IE,          RU_EN,  RU_GHE,  RU_SHA,   RU_ZE,   RU_HA,
-       RU_EF, RU_YERU,   RU_VE,    RU_A,   RU_PE,          RU_ER,    RU_O,   RU_EL,   RU_DE,  RU_ZHE,
-       RU_YA,  RU_CHE,   RU_ES,   RU_EM,    RU_I,          RU_TE, RU_SOFT,   RU_BE,   RU_YU,    RU_E,
-                DF_EN, SW_LANG,  KC_SPC,  LA_NAV,         LA_SYM, KC_LSFT,  LA_ALT,  _______
+    _______,_______,_______,_______,_______,       _______,_______,_______,  RU_ZE,  RU_HA,
+    _______,_______,_______,_______,_______,       _______,_______,_______,_______, RU_ZHE,
+    _______,_______,_______,_______,_______,       _______,_______,  RU_BE,  RU_YU,   RU_E,
+            _______,_______,_______,_______,       _______,_______,_______,  DF_EN
     ),
 
     [_SYM] = LAYOUT_all(
-      //  _______,      _______,      _______,     _______,     _______,          _______,      _______,          _______,       _______,         _______,
-          KC_EXLM,        KC_AT,       KC_HASH,    KC_DOLLAR, KC_PERCENT,    KC_CIRCUMFLEX,  KC_QUESTION,     KC_ASTERISK, KC_LEFT_PAREN,  KC_RIGHT_PAREN,
-          KC_TILDE,    KC_GRAVE,     KC_QUOTE, S(KC_QUOTE),     KC_MINUS,          KC_PLUS,      OS_SHFT,         OS_CTRL,        OS_ALT,          OS_CMD,
-      KC_BACKSLASH,     KC_PIPE, KC_SEMICOLON,    KC_COMMA,  S(KC_MINUS),      KC_KP_EQUAL,       KC_DOT, S(KC_SEMICOLON),       KC_PIPE,        KC_SLASH,
-                        _______,      _______,     _______,      _______,          _______,      _______,         _______,       _______
+    KC_EXLM,  KC_AT,KC_HASH, KC_DLR,KC_PERC,       KC_CIRC,KC_QUES,KC_ASTR,KC_LPRN,KC_RPRN,
+    KC_TILD, KC_GRV,KC_QUOT, KC_DQT,KC_MINS,       KC_PLUS,OS_SHFT,OS_CTRL, OS_ALT, OS_CMD,
+    KC_BSLS,KC_PIPE,KC_SCLN,KC_COMM,KC_USCR,        KC_EQL, KC_DOT, KC_CLN,KC_PIPE,KC_SLSH,
+            _______,_______,_______,_______,       _______,_______,_______,_______
     ),
 
     [_NAV] = LAYOUT_all(
-      SW_TAB,  SW_WIN,    TAB_L,    TAB_R,   KC_GRV,         KC_ESC,  KC_HOME,  KC_END,   KC_BSPC,   KC_DEL,
-      OS_CMD,  OS_ALT,  OS_CTRL,  OS_SHFT,   KC_ENT,         KC_ENT,  KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,
-     SPACE_L,   SPC_R,  _______,  KC_PSCR,   KC_TAB,         KC_TAB,  KC_PGUP,  KC_PGDN,  XXXXXXX,  XXXXXXX,
-              _______,  _______,  _______,  _______,        _______,  _______,  _______,  _______
-    ),
+     SW_TAB, SW_WIN,  TAB_L,  TAB_R, KC_GRV,        KC_ESC,KC_HOME, KC_END,KC_BSPC, KC_DEL,
+     OS_CMD, OS_ALT,OS_CTRL,OS_SHFT, KC_ENT,        KC_ENT,KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,
+    SPACE_L,  SPC_R,_______,KC_PSCR, KC_TAB,        KC_TAB,KC_PGUP,KC_PGDN,XXXXXXX,XXXXXXX,
+            _______,_______,_______,_______,       _______,_______,_______,_______
+    )
 
     [_NUM] = LAYOUT_all(
-     KC_P1,   KC_P2,    KC_P3,   KC_P4,   KC_P5,          KC_P6,    KC_P7,    KC_P8,    KC_P9,    KC_P0,
-    OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT,  KC_F11,         KC_F12,  OS_SHFT,  OS_CTRL,   OS_ALT,   OS_CMD,
-     KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,          KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,
-            _______, _______, _______, _______,         _______,  _______,  _______,  _______
+      KC_P1,  KC_P2,  KC_P3,  KC_P4,  KC_P5,         KC_P6,  KC_P7,  KC_P8,  KC_P9,  KC_P0,
+     OS_CMD, OS_ALT,OS_CTRL,OS_SHFT, KC_F11,        KC_F12,OS_SHFT,OS_CTRL, OS_ALT, OS_CMD,
+      KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,         KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10,
+            _______,_______,_______,_______,       _______,_______,_______,_______
     ),
 
     [_ALT] = LAYOUT_all(
     _______,_______,_______,_______,  RU_YO,       _______,_______,RU_SHCH,_______,_______,
     _______,_______,_______,_______,_______,       _______,_______,_______,_______,_______,
     _______,_______,_______,_______,_______,       _______,RU_HARD,_______,_______,_______,
+            _______,_______,_______,_______,       _______,_______,_______,_______
+    ),
+
+    [_MOUSE] = LAYOUT_all(
+    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,  RESET,       KC_BTN1,KC_BTN2,KC_BTN3,KC_BTN4,KC_BTN5,
+    KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,XXXXXXX,       XXXXXXX,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,
+    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,       XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
             _______,_______,_______,_______,       _______,_______,_______,_______
     ),
 };
