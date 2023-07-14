d1daabab0acebbefe69eae1e19f04b73185d25f0
vedmalex <vedmalex@gmail.com>
2023-07-14 22:18:34 +0300

    implement language fix

--- keymap.c
+++ keymap.c
@@ -11,7 +11,7 @@
 #define LA_ALT MO(_ALT)
 
 #define DF_RU DF(_RU)
-#define DF_EN DF(_EN) oklkjyuuoynmnjnjh
+#define DF_EN DF(_EN)
 #define DF_MOUSE TG(_MOUSE)
 
 #define QUOT S(KC_GRV)
@@ -29,6 +29,9 @@
 #define TAB_R C(KC_TAB)
 
 #define LSW C(KC_SPC)
+
+#define KC_USCR S(KC_MINS)
+#define KC_CLN S(KC_SCLN)
 
 enum layers {
     _EN,
@@ -50,6 +53,7 @@ enum keycodes {
     SW_WIN,       // Switch to next window         (alt-tab)
     SW_TAB,       // Switch to next browser tab    (ctrl-tab)
     SW_LANG,      // Switch layout in macos (ctrl-space)
+    SW_LNFX,      // fix layer
     SW_UNDO_REDO, // ctrl+z
     SW_FORWARD,   // ctrl + -
     SAVE,         // CMD+S
@@ -60,38 +64,35 @@ enum keycodes {
     JS_ARROW,     // =>
 };
 
-#define KC_DQT S(KC_QUOT)
-#define KC_USCR S(KC_MINS)
-#define KC_CLN S(KC_SCLN)
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_EN] = LAYOUT_all(
       KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,
       KC_A,    KC_S,   KC_D,   KC_F,   KC_G,          KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,
-      KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,          KC_N,   KC_M,S(KC_7),KC_LBRC,KC_RBRC,
-           DF_MOUSE,SW_LANG, KC_SPC, LA_NAV,        LA_SYM,KC_LSFT, LA_ALT,  DF_RU
+      KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,          KC_N,   KC_M,KC_AMPR,KC_LBRC,KC_RBRC,
+           DF_MOUSE,SW_LANG, KC_SPC, LA_NAV,        LA_SYM,KC_LSFT, LA_ALT,SW_LNFX
     ),
 
     [_RU] = LAYOUT_all(
     _______,_______,_______,_______,_______,       _______,_______,_______,  RU_ZE,  RU_HA,
     _______,_______,_______,_______,_______,       _______,_______,_______,_______, RU_ZHE,
     _______,_______,_______,_______,_______,       _______,_______,  RU_BE,  RU_YU,   RU_E,
-            _______,_______,_______,_______,       _______,_______,_______,  DF_EN
+            _______,_______,_______,_______,       _______,_______,_______,_______
     ),
 
     [_SYM] = LAYOUT_all(
     KC_EXLM,  KC_AT,KC_HASH, KC_DLR,KC_PERC,       KC_CIRC,KC_QUES,KC_ASTR,KC_LPRN,KC_RPRN,
-    KC_TILD, KC_GRV,KC_QUOT, KC_DQT,KC_MINS,       KC_PLUS,OS_SHFT,OS_CTRL, OS_ALT, OS_CMD,
+    KC_TILD, KC_GRV,KC_QUOT,KC_DQUO,KC_MINS,       KC_PLUS,OS_SHFT,OS_CTRL, OS_ALT, OS_CMD,
     KC_BSLS,KC_PIPE,KC_SCLN,KC_COMM,KC_USCR,        KC_EQL, KC_DOT, KC_CLN,KC_PIPE,KC_SLSH,
             _______,_______,_______,_______,       _______,_______,_______,_______
     ),
 
     [_NAV] = LAYOUT_all(
      SW_TAB, SW_WIN,  TAB_L,  TAB_R, KC_GRV,        KC_ESC,KC_HOME, KC_END,KC_BSPC, KC_DEL,
-     OS_CMD, OS_ALT,OS_CTRL,OS_SHFT, KC_ENT,        KC_ENT,KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,
+     OS_CMD, OS_ALT,OS_CTRL,OS_SHFT, KC_ENT,        KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,KC_ENT,
     SPACE_L,  SPC_R,_______,KC_PSCR, KC_TAB,        KC_TAB,KC_PGUP,KC_PGDN,XXXXXXX,XXXXXXX,
             _______,_______,_______,_______,       _______,_______,_______,_______
-    )
+    ),
 
     [_NUM] = LAYOUT_all(
       KC_P1,  KC_P2,  KC_P3,  KC_P4,  KC_P5,         KC_P6,  KC_P7,  KC_P8,  KC_P9,  KC_P0,
@@ -120,6 +121,7 @@ bool is_oneshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
         case LA_SYM:
         case LA_NAV:
+        case LA_ALT:
             return true;
         default:
             return false;
@@ -130,6 +132,7 @@ bool is_oneshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
         case LA_SYM:
         case LA_NAV:
+        case LA_ALT:
         case KC_LSFT:
         case OS_SHFT:
         case OS_CTRL:
@@ -151,8 +154,7 @@ oneshot_state os_cmd_state  = os_up_unqueued;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     update_swapper(&sw_win_active, KC_LALT, KC_TAB, SW_WIN, OS_SHFT, keycode, record);
-    update_swapper(&sw_tab_active, KC_LCTL, KC_TAB, SW_TAB, OS_SHFT, keycode, record);
-    // update_swapper(&sw_tab_active, KC_LCTL, KC_TAB, SW_TAB, OS_SHFT, keycode, record);
+    update_swapper(&sw_tab_active, KC_LGUI, KC_TAB, SW_TAB, OS_SHFT, keycode, record);
     update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record);
     update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record);
     update_oneshot(&os_alt_state, KC_LALT, OS_ALT, keycode, record);
@@ -174,6 +176,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
         }
+        case SW_LNFX: {
+            if (record->event.pressed) {
+                if (get_highest_layer(layer_state) == _EN) {
+                    layer_clear();
+                    layer_on(_RU);
+                } else {
+                    layer_clear();
+                    layer_on(_EN);
+                }
+            }
+            return false;
+
+        } // Switch layout in macos (ctrl-space)
+
             // case JS_ARROW: {
             //     if (record->event.pressed) {
             //         SEND_STRING("=>");
